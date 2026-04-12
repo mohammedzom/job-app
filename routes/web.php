3d758bc32ae96131f57525e6447ca48fe39c20ee
mohammedzom <mohammedzomlot2@gmail.com>
2026-04-12 16:51:16 +0300

    chore: update APP_NAME in .env.example to Shaghalni

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,6 @@ Route::middleware(['auth', 'checkrole:admin,job_seeker'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::resource('resumes', ResumesController::class)->names('resumes')->except(['edit', 'update']);
 });
 
